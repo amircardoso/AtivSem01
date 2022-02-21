5c7c589e81d64fa895af6382b61b0a2a2505ae1a
Amir Chaves Cardoso <cardoso-amir@outlook.com>
2022-02-20 23:00:48 -0300

    AtivSem01

---
+++ AtivSem01/Cadastros.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtivSem01
+{
+    interface IOperacoesBD
+    {
+        void Inserir();
+        void Alterar();
+        void Deletar();
+        void Pesquisar();
+    }
+
+    abstract class Cadastros : IOperacoesBD
+       {
+        #region IOperacoesBD
+        public void Inserir()
+        {
+            Console.WriteLine("Executando o método Inserir da classe Cadastros para IOperacoesBD");
+        }
+
+        public void Alterar()
+        {
+            Console.WriteLine("Executando o método Alterar da classe Cadastros para IOperacoesBD");
+        }
+
+        public void Deletar()
+        {
+            Console.WriteLine("Executando o método Deletar da classe Cadastros para IOperacoesBD");
+        }
+
+        public void Pesquisar()
+        {
+            Console.WriteLine("Executando o método Pesquisar da classe Cadastros para IOperacoesBD");
+        }
+
+        {
+          private readonly string _Conexao                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ;
+        }
+
+    public class Clientes;
+    public class Pedidos;
+}
---
+++ AtivSem01/Funcoes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtivSem01
+{
+    class Funcoes
+    {
+
+        try
+
+        internal class maiorValor
+        {
+            int n = 0, maior = 0;
+            private static object Lista;
+            readonly List<int> lista = new List<int> { 2, 4, 6, 8 };
+            public int MaiorValor => maiorValor;
+            Console.WriteLine(maiorValor)
+        }
+        
+        catch (Exception)
+
+        {
+
+            lblMensagem.Text = "0";
+
+        }
+        
+        try 
+
+        internal class soma
+        {
+            int valores[] = { 1, 3, 5, 7, 9 };
+            int soma = 0;
+            int i;
+
+            for(i=0; i<5;i++){
+                soma = soma + valores [i];
+            
+            Console.WriteLine("A soma dos valores dos é: " + soma);
+        }
+
+        catch (Exception)
+
+        {
+        
+            lblMensagem.Text = "0";
+        
+    }
+
+       
+}
